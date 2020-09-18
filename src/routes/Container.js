a02bd9c65d40185d609ab93573d273fbe33254d1
Souvikns <souvikde.ns@gmail.com>
2020-09-18 23:13:36 +0530

    added react client

--- src/routes/Container.js
+++ src/routes/Container.js
@@ -6,8 +6,10 @@ router.route('/')
      * Create a container
      * User needs to be logged in the website or client to create 
      * a container
+     * 
      * Client will send json web token to create authenticate. 
-     * We can also verify and validate the user who wants to create the container.
+     * We can also verify and validate the user who wants to 
+     * create the container using jwt. 
      * 
      */
     .post()
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const app = require('./app')
 const mongoose = require('mongoose')
 
+const PORT = process.env.PORT || 5000
+
 mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true
 }).then(() => {
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
         console.log('server started at port 3000')
     })
 }).catch(err => {
