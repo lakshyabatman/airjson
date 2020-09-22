7d5ef1384a48b3a31b57b566a6f9d7fc0c8e2466
Souvik <souvikde.ns@gmail.com>
2020-09-22 19:18:35 +0530

    added @types/mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
 // importing routes 
 import apiRoute from './routes'
+
+dotenv.config()
 
 const app = express();
 
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import app from './app'
+import mongoose from 'mongoose'
 
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server started at port ${process.env.PORT || 5000}`)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server started at port ${process.env.PORT || 5000}`)
+    })
+}).catch(err => {
+    console.log(err)
 })
