0ea48ac6731232d568f18221dcbe33cabdfaa0e4
Souvikns <souvikde.ns@gmail.com>
2020-09-23 02:27:50 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import './passport'
 import passport from 'passport'
+dotenv.config();
+import './passport'
+
+
 
 // importing routes 
 import apiRoute from './routes';
 
-dotenv.config();
+
 
 const app = express();
 app.use(passport.initialize())
