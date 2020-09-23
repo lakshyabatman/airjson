1f6703be2c5870ba3780718f3d0f62e37fdc4126
Souvik <souvikde.ns@gmail.com>
2020-09-23 14:09:02 +0530

    adding auth route 😎

---
+++ src/routes/auth.ts
@@ -0,0 +1,11 @@
+import {Router} from 'express'
+
+const router = Router()
+
+router.route('/login')
+
+
+router.route('/signup')
+
+
+export default router
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import express from 'express';
 // importing sub routes
 import docRoute from './documents';
 import containerRoute from './container'
+import authRoute from './auth'
 
 const router = express.Router();
 
 router.use('/document', docRoute);
 router.use('/container', containerRoute)
+router.use('/auth', authRoute)
 
 
 export default router;
